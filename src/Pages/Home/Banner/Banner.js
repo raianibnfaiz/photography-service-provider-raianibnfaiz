c8cb9bf84eb78defdd1a6cd38cbdea5299ba58c3
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2022-06-13 13:45:34 +0600

    banner and title added

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Banner.css';
+const Banner = () => {
+
+    return (
+        <div>
+            <div className="bg">
+                <div className="title-section">
+                    <h1 className="title">Independent</h1>
+
+                </div>
+            </div>
+        </div>
+
+        // <div className="container">
+        //     <img src="https://i.ibb.co/t8RGPVY/robert-shunev-m-S1nl-Ybq1k-A-unsplash.jpg" width="1349px" alt="" />
+        //     <h3 className='text-center text-purple-400'> Photography</h3>
+        // </div>
+    );
+};
+
+export default Banner;
